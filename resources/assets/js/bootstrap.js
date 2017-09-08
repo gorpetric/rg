98c951e89c510fb5d4d70c1e632b2beeafc72ea8
Goran <gorpetric@foi.hr>
2017-09-08 15:17:46 +0200

    Laravel 5.5 update

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -2,8 +2,15 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
-window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.RG.csrfToken;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
