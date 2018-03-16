e3c89f2c217683f754af06d4d52a9d4ce9c9ca55
Goran <gorpetric@foi.hr>
2018-03-16 09:17:13 +0100

    Update dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,6 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .browserSync('rg.goran');
+   .browserSync('rg.test');
 
 mix.disableNotifications();
