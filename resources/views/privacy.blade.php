8393e0b477e838fe221365f2cb7fea972eabc2ba
Goran <booowya@gmail.com>
2018-03-17 15:40:11 +0100

    Create privacy.blade.php

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('title'){{ 'Privacy policy' }}@stop
+@section('description'){{ 'Royal Gym Brezje - privacy policy' }}@stop
+
+@section('content')
+<div class='container'>
+    <h1>Privacy policy</h1>
+</div>
+@stop
